f12cb9acf400adfc5acdb369a171255246acc0df
Samilly <samillynunes19@gmail.com>
2024-05-18 21:59:37 -0300

    ui: reveal answers

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -15,18 +15,35 @@ export default function Answer(props: AnswerProps){
     return (
         <div className={styles.answer} onClick={()=> props.onResponse(props.index)}>
             <div className={styles.content} >
-                <div className={styles.front} >
-                    <div className={styles.letter} style={{backgroundColor: props.color }} >
-                        {props.letter}
-                    </div>
-                    <div className={styles.value}>
-                        {answer.value}
-                    </div>
+                {!answer.isRevealed ? (
+                    <div className={styles.front} >
+                        <div className={styles.letter} style={{backgroundColor: props.color }} >
+                            {props.letter}
+                        </div>
+                        <div className={styles.value}>
+                            {answer.value}
+                        </div>
 
-                </div>
-                <div className={styles.back} >
-
-                </div>
+                    </div>
+                ) : (
+                    <div className={styles.back} >
+                        {answer.isRight ? (
+                            <div className={styles.right}>
+                                <div>A resposta certa é ...</div>
+                                <div className={styles.value}> {answer.value} </div>
+                            </div>
+                        ) : (
+                            <div className={styles.wrong}>
+                                <div>A resposta informada está errada ...</div>
+                                <div className={styles.value}> {answer.value} </div>
+                            </div>
+                        )}
+                        
+                        
+                    </div>
+                )}
+                
+                
             </div>
         </div>
     );
