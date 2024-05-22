cf78ebf8908aa6659e7d098f2aedbb67bac379de
Samilly <samillynunes19@gmail.com>
2024-05-22 16:09:40 -0300

    animation: flip animation to answer card added

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -11,37 +11,38 @@ interface AnswerProps{
 
 export default function Answer(props: AnswerProps){
     const answer = props.value;
+    const answerRevealedStyled = answer.isRevealed ? styles.answerRevealed : '';
 
     return (
         <div className={styles.answer} onClick={()=> props.onResponse(props.index)}>
-            <div className={styles.content} >
-                {!answer.isRevealed ? (
-                    <div className={styles.front} >
-                        <div className={styles.letter} style={{backgroundColor: props.color }} >
-                            {props.letter}
-                        </div>
-                        <div className={styles.value}>
-                            {answer.value}
-                        </div>
+            <div className={`${answerRevealedStyled} ${styles.content}`} >
+                
+                <div className={styles.front} >
+                    <div className={styles.letter} style={{backgroundColor: props.color }} >
+                        {props.letter}
+                    </div>
+                    <div className={styles.value}>
+                        {answer.value}
+                    </div>
 
-                    </div>
-                ) : (
-                    <div className={styles.back} >
-                        {answer.isRight ? (
-                            <div className={styles.right}>
-                                <div>A resposta certa é ...</div>
-                                <div className={styles.value}> {answer.value} </div>
-                            </div>
-                        ) : (
-                            <div className={styles.wrong}>
-                                <div>A resposta informada está errada ...</div>
-                                <div className={styles.value}> {answer.value} </div>
-                            </div>
-                        )}
-                        
-                        
-                    </div>
-                )}
+                </div>
+            
+                <div className={styles.back} >
+                    {answer.isRight ? (
+                        <div className={styles.right}>
+                            <div>A resposta certa é ...</div>
+                            <div className={styles.value}> {answer.value} </div>
+                        </div>
+                    ) : (
+                        <div className={styles.wrong}>
+                            <div>A resposta informada está errada ...</div>
+                            <div className={styles.value}> {answer.value} </div>
+                        </div>
+                    )}
+                    
+                    
+                </div>
+                
                 
                 
             </div>
